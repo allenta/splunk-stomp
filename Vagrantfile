7155235a70a414cdd8e45ea178a99b1cbedab8b9
Allenta Consulting <info@allenta.com>
2013-04-22 16:06:25 +0200

    Upgraded Vagranfile version + minor Puppet manifest improvements

--- Vagrantfile
+++ Vagrantfile
@@ -1,21 +1,25 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-Vagrant::Config.run do |config|
+Vagrant.configure('2') do |config|
   config.vm.box = 'precise64'
-  config.vm.host_name = 'dev'
+  config.vm.hostname = 'dev'
   config.vm.box_url = 'http://files.vagrantup.com/precise64.box'
+
+  config.vm.provider :virtualbox do |vb|
+    vb.customize [
+      'modifyvm', :id,
+      '--memory', '512',
+      '--name', 'Splunk STOMP',
+    ]
+  end
 
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = 'vagrant/manifests'
   end
 
-  config.vm.customize [
-    'modifyvm', :id,
-    '--memory', '512',
-    '--name', 'Splunk STOMP',
-  ]
-
-  config.vm.forward_port 8000, 8000
-  config.vm.forward_port 8089, 8089
+  config.vm.network :forwarded_port, guest: 8000, host: 8000
+  config.vm.network :forwarded_port, guest: 8089, host: 8089
+  config.vm.network :forwarded_port, guest: 15672, host: 15672
+  config.vm.network :forwarded_port, guest: 55672, host: 55672
 end
--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -1,25 +1,51 @@
-class phase1 {
+Exec {
+  path => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/']
+}
+
+class system-update {
   exec {'apt-get-update':
     command => '/usr/bin/apt-get update'
   }
 
-  package {['python', 'python-pip', 'rabbitmq-server', 'rabbitmq-stomp']:
+  package {['python', 'python-pip']:
     ensure => present,
     require => Exec['apt-get-update'],
   }
 }
 
-class phase2 {
-  Class['phase1'] -> Class['phase2']
+class rabbitmq {
+  Class['system-update'] -> Class['rabbitmq']
+
+  exec {'add-rabbitmq-source':
+    user => 'root',
+    creates => "/home/vagrant/.vagrant.add-rabbitmq-source",
+    command => "echo \"deb http://www.rabbitmq.com/debian/ testing main\" >> /etc/apt/sources.list && wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc -O /tmp/rabbitmq-signing-key-public.asc && apt-key add /tmp/rabbitmq-signing-key-public.asc && touch /home/vagrant/.vagrant.add-rabbitmq-source",
+  }
+
+  exec {'apt-get-update-rabbitmq':
+    command => '/usr/bin/apt-get update',
+    require => Exec['add-rabbitmq-source'],
+  }
+
+  package {['rabbitmq-server']:
+    ensure => present,
+    require => Exec['apt-get-update-rabbitmq'],
+  }
+
+  exec {'enable-rabbitmq-stomp-and-management':
+    user => 'root',
+    creates => "/home/vagrant/.vagrant.enable-rabbitmq-stomp-and-management",
+    command => "rabbitmq-plugins enable rabbitmq_management && rabbitmq-plugins enable rabbitmq_stomp && touch /home/vagrant/.vagrant.enable-rabbitmq-stomp-and-management",
+    require => Package['rabbitmq-server'],
+  }
+}
+
+class user {
+  Class['system-update'] -> Class['user']
 
   package {['virtualenv']:
     ensure   => 'installed',
     provider => 'pip',
-  }
-
-  service {['rabbitmq-server']:
-    enable => true,
-    ensure => running,
   }
 
   exec {'create-virtualenv':
@@ -59,5 +85,6 @@ class phase2 {
   }
 }
 
-include phase1
-include phase2
+include system-update
+include rabbitmq
+include user
